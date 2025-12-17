038b32170989d9f48cb60dbe7afc193efe1c76c2
Devang-shah2025 <23010101248@darshan.ac.in>
2025-12-17 17:02:55 +0530

    Implement database models

---
+++ database/index.ts
@@ -0,0 +1,11 @@
+/**
+ * Central export point for all database models
+ * Import models from this file throughout the application
+ */
+
+export { default as Event } from './event.model';
+export { default as Booking } from './booking.model';
+
+// Export TypeScript interfaces for type checking
+export type { IEvent } from './event.model';
+export type { IBooking } from './booking.model';
